a25d18c85d74d76893a10100f242ad5d52791116
paddybishop <paddybishop@gmail.com>
2013-02-02 21:05:37 +0000

    Update myCode.js

--- myCode.js
+++ myCode.js
@@ -1,30 +1,31 @@
 code = function () {
 
-    function get_random_color() {
-	return '#' + Math.random().toString(16).substring(4);
-	}
-	
-    var Particle = {};
-	
+    var particleGenerator = function() {
+        return {
+            x: Math.random()*800,
+            y: Math.random()*600,
+            radius: Math.random()*15,
+            color: 'blue'
+        };
+    };
     
-	
-	var numberOfParticles = 0;
-	
-	while(numberOfParticles < 500) {
-	
-	
-	Particle.y = Math.random()*600;
-	Particle.x = Math.random()*800;
-	Particle.radius = Math.random()*60;
-	Particle.color = get_random_color();
-	page.draw(Particle);
-	
-	numberOfParticles = numberOfParticles + 1;
-	
-	}
-	
-	
-	
+    var maxNumberOfParticles = 500;
+    var particleArray = [];
     
+    while (particleArray.length < maxNumberOfParticles) {
+        var newParticle = particleGenerator();
+        particleArray.push(newParticle);
+    }
     
+    page.perFrame = function (delta) {
+        page.clear();
+
+        particleArray.forEach(function (p) {
+            if (p.y < 600) {
+                p.y = p.y + (0.3 * p.radius * delta);
+            }
+            page.draw(p);
+        });
+    };
 };
+
