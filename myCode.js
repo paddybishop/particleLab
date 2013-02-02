ce3d3b5c148c5f255e3ff373204cb66f6c63964e
paddybishop <paddybishop@gmail.com>
2013-02-02 21:23:30 +0000

    Update myCode.js

--- myCode.js
+++ myCode.js
@@ -1,11 +1,15 @@
-code = function () {
+code = function get_random_color() {
+    return '#' + Math.random().toString(16).substring(4);
+    }
+
+function () {
 
     var particleGenerator = function() {
         return {
             x: Math.random()*800,
             y: Math.random()*600,
             radius: Math.random()*15,
-            color: 'blue'
+            color: get_random_color(),
         };
     };
     
@@ -28,3 +32,24 @@ code = function () {
         });
     };
 };
+
+
+
+    var Particle = {};
+
+    
+
+	var numberOfParticles = 0;
+
+	while(numberOfParticles < 500) {
+
+
+	Particle.y = Math.random()*600;
+	Particle.x = Math.random()*800;
+	Particle.radius = Math.random()*30;
+	Particle.color = get_random_color();
+	page.draw(Particle);
+
+	numberOfParticles = numberOfParticles + 1;
+
+	}
