b4a52d22c2aec234cb16a9dac5cdee04323dc4f0
paddybishop <paddybishop@gmail.com>
2013-02-02 20:44:26 +0000

    Update myCode.js

--- myCode.js
+++ myCode.js
@@ -1,29 +1,33 @@
 code = function () {
 
-    var orangeParticle = {
+    function get_random_color() {
+	return '#' + Math.random().toString(16).substring(4);
+	}
+	
+    var Particle = {
         x: 425,
         y: 300,
         radius: 25,
-        color: 'orange',
+        color: get_random_color(),
     };
+	
     
-    var blackParticle = {
-        x: 375,
-        radius: 25,
-        y: 300,
-        color: 'black',
-    };
-
-    page.draw(orangeParticle);
-    page.draw(blackParticle);
+	
+	var numberOfParticles = 0;
+	
+	while(numberOfParticles < 500) {
+	
+	
+	Particle.y = Math.random()*600;
+	Particle.x = Math.random()*800;
+	Particle.radius = Math.random()*30;
+	page.draw(Particle);
+	numberOfParticles = numberOfParticles + 1;
+	
+	}
+	
+	
+	
     
-    page.perClick = function (mouse) {
-        page.clear();
- 
-        orangeParticle.y = mouse.y;
-        orangeParticle.x = mouse.x;
-        
-        page.draw(blackParticle);
-        page.draw(orangeParticle);
-    }
+    
 };
