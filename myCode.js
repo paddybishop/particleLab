df850ffcc5b5180aa9262bdec9a64dee9812a040
paddybishop <paddybishop@gmail.com>
2013-02-02 20:53:03 +0000

    Update myCode.js

--- myCode.js
+++ myCode.js
@@ -20,7 +20,7 @@ code = function () {
 	Particle.y = Math.random()*600;
 	Particle.x = Math.random()*800;
 	Particle.radius = Math.random()*30;
-	Particle.color = color:get_random_color();
+	Particle.color = get_random_color();
 	page.draw(Particle);
 	
 	numberOfParticles = numberOfParticles + 1;
